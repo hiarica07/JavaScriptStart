e94c4cdc879375b8e641ddbe4d94147cdffbe454
hiarica07 <halilarica@gmail.com>
2024-05-03 16:44:34 +0300

    f

---
+++ my-works/2-Operatorler1/2-script.js
@@ -0,0 +1,19 @@
+let sonuc;
+let a = 10, b = 20, c = 30;
+
+// 1-Aritmetik Operatörler
+
+sonuc = a + b;
+sonuc = a - b;
+sonuc = a / b;
+sonuc = a * b;
+sonuc = c % a;
+
+// 2- Atama Operatörleri
+
+// 3- Karşılaştırma Operatörleri
+
+// 4-Mantıksal Operatörler
+
+
+console.log(sonuc);
