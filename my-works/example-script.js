3fe592909d12e2e40f83a5df5c4bc448d2dc3527
hiarica07 <halilarica@gmail.com>
2024-05-03 16:29:38 +0300

    İkinci part

---
+++ my-works/example-script.js
@@ -0,0 +1,39 @@
+let ogr1_ad = "Halil";
+let ogr1_soyad ="Arıca";
+let ogr1_dogumTarihi ="1996"
+
+let ogr1_mat1 = 70;
+let ogr1_mat2 = 70;
+let ogr1_mat3 = 80;
+
+let ogr1_ortalama = (ogr1_mat1 + ogr1_mat2 + ogr1_mat3) / 3 ;
+
+console.log(parseFloat(ogr1_ortalama)); // ParseFloat virgülleri de gösterir.
+console.log(ogr1_ortalama >= 50);
+
+let ogr2_ad = "Emre";
+let ogr2_soyad ="Arıca";
+let ogr2_dogumTarihi ="2013"
+
+let ogr2_mat1 = 40;
+let ogr2_mat2 = 40;
+let ogr2_mat3 = 50;
+
+let ogr2_ortalama = (ogr2_mat1 + ogr2_mat2 + ogr2_mat3) / 3;
+
+console.log(parseFloat(ogr2_ortalama));
+console.log(ogr2_ortalama >=50);
+
+let suankiYil = new Date().getFullYear();
+
+let ogr1_yas = suankiYil - parseInt(ogr1_dogumTarihi);   // ParseInt tam sayi belirtir.
+let ogr2_yas = suankiYil - parseInt(ogr2_dogumTarihi);
+
+console.log(ogr1_yas);
+console.log(ogr2_yas);
+
+console.log(`Birinci ogrenci ${ogr1_ad} ${ogr1_soyad} ${suankiYil - ogr1_dogumTarihi} yasindadir. Sınav ortalaması ${ogr1_ortalama} durumu ${ogr1_ortalama >=50} `);
+console.log(`İkinci ogrenci ${ogr2_ad} ${ogr2_soyad} ${suankiYil - ogr2_dogumTarihi} yasindadir. Sınav ortalaması ${ogr2_ortalama} durumu ${ogr2_ortalama >=50} `);
+
+
+
--- my-works/script.js
+++ my-works/script.js
@@ -4,8 +4,33 @@ let urunFiyat=15000;  // number
 console.log(typeof urunAdi);
 console.log(typeof urunFiyat);
 
+// let sayi1="10";
+// let sayi2="10";
+
+// console.log(Number(sayi1) + Number(sayi2));
+
 let sayi1="10";
 let sayi2="10";
+console.log(sayi1.toString() + sayi2.toString());
+
+
+let isim ="Halil"
+let soyad ="Arica"
+
+console.log(isim + " " + soyad);
+
+let sinavNotu = 70;
+let basarilimi = (sinavNotu >= 50);
+
+console.log(basarilimi);
+console.log(typeof basarilimi); // boolean
+
+let yas;
+console.log(yas);
+console.log(typeof yas); // bu sekilde undefined
+
+yas = ""; // bu sekilde olursa boşluk, string
+
 
 
 
