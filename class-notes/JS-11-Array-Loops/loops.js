d19cdcd9283f2cbfe02c7fa59cdd0e7691f1793a
hiarica07 <halilarica@gmail.com>
2024-05-27 11:39:44 +0300

    a

--- class-notes/JS-11-Array-Loops/loops.js
+++ class-notes/JS-11-Array-Loops/loops.js
@@ -30,3 +30,28 @@ const dizilereAyir1=()=>{
 }
 
 dizilereAyir1()
+
+
+//********* FOR IN */
+
+
+const dizi2=[-5,15,-78,-45,34,27,4,-9,7]
+
+const negatifler2=[]
+const pozitifler2=[]
+
+const dizilereAyir2=()=>{
+    
+    for (let i in dizi2){
+        if(dizi2[i]<0){
+            negatifler2.push(dizi2[i])
+        }else{
+            pozitifler2.push(dizi2[i])
+        }
+    }
+
+    console.log(pozitifler2);
+    console.log(negatifler2);
+}
+
+dizilereAyir2()
